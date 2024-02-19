45d23a0a85d48a442dfccb814039a2f8df1f10be
talha <talha18513@gmail.com>
2024-02-19 16:52:47 +0500

    first commit

--- js/Home.js
+++ js/Home.js
@@ -18,14 +18,7 @@ function toggleResMenu() {
 }
 
 // Open the full screen search box
-function openSearch() {
-  document.getElementById("myOverlay").style.display = "block";
-}
 
-// Close the full screen search box
-function closeSearch() {
-  document.getElementById("myOverlay").style.display = "none";
-}
 
 $(document).ready(function () {
   $('.center').slick({
--- js/Nav.js
+++ js/Nav.js
@@ -1,17 +1,26 @@
 function toggleMenu() {
-    const menu = document.getElementById("res_menu")
-    if (menu.style.display === "none") {
-      menu.style.display = "block";
-    } else {
-      menu.style.display = "none";
-    }
+  const menu = document.getElementById("res_menu")
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
   }
-  
-  function toggleResMenu() {
-    const resmenu = document.getElementById("res_dropdown")
-    if (resmenu.style.display === "none") {
-      resmenu.style.display = "block";
-    } else {
-      resmenu.style.display = "none";
-    }
+}
+
+function toggleResMenu() {
+  const resmenu = document.getElementById("res_dropdown")
+  if (resmenu.style.display === "none") {
+    resmenu.style.display = "block";
+  } else {
+    resmenu.style.display = "none";
   }
+}
+
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
